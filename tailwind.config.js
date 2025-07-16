a12d31bd13494d033d3280ee77076f20a94db39c
Rolandas <rzabulis@yahoo.co.uk>
2025-07-16 18:31:46 +0300

    Dev: installed Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -15,7 +16,27 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        // todo: add my custom colors
+        // colors: {
+        //     primary: {
+        //         DEFAULT: '#1D4ED8',
+        //         dark: '#1E40AF',
+        //         light: '#3B82F6',
+        //     },
+        //     secondary: {
+        //         DEFAULT: '#F97316',
+        //         dark: '#B45309',
+        //         light: '#FB923C',
+        //     },
+        //     gray: {
+        //         700: '#374151',
+        //         800: '#1F2937',
+        //     },
+        // },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin'),
+    ],
 };
