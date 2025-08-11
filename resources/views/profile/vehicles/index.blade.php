f59b15aa6b847a48cf0dff5f45d65369b05f9bfd
Rolandas <rzabulis@yahoo.co.uk>
2025-08-11 15:34:09 +0300

    Dev: add Remove UserVehicle in view

--- resources/views/profile/vehicles/index.blade.php
+++ resources/views/profile/vehicles/index.blade.php
@@ -7,8 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto lg:px-8 px-8">
+            @if (session('success'))
+                <div class="flex items-center justify-between mb-4 p-4 text-sm text-green-800 bg-green-100 rounded" role="alert">
+                    <span>{{ session('success') }}</span>
+                    <button type="button" onclick="this.parentElement.remove()" class="text-green-800 hover:text-green-900">
+                        &times;
+                    </button>
+                </div>
+            @endif
             <div class="grid grid-cols-4 gap-4">
-                @foreach($vehicles as $vehicle)
+            @foreach($vehicles as $vehicle)
 {{--                    <a href="{{ route('profile.vehicles.show', $vehicle) }}">--}}
                         <div class="bg-white shadow-lg rounded-lg p-4 mb-5">
                             <p class="font-semibold mb-1">
@@ -23,7 +31,15 @@
                             <p class="mb-2">
                                 <a href="{{ route('profile.vehicles.edit', $vehicle) }}">Edit</a>
                             </p>
-                            @if($vehicle->photo)
+
+                            <form action="{{ route('profile.vehicles.destroy', $vehicle) }}" method="POST" onsubmit="return confirm('Are you sure you want to remove this vehicle?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-600 hover:underline">Remove</button>
+                            </form>
+
+
+                        @if($vehicle->photo)
 {{--                                <img src="{{ $vehicle->photo }}" alt="Photo" class="w-full h-48 object-cover rounded">--}}
                                 <img src="{{ asset('storage/' . $vehicle->photo) }}" alt="Photo" class="w-full h-48 object-cover rounded" >
                             @endif
