2637a9e23ba96f04026bbb870b5316e0de4acb20
Rolandas <rzabulis@yahoo.co.uk>
2025-09-25 14:20:45 +0300

    Dev: cleanup - add support for the canceled rides tab in the My Rides view

--- resources/views/profile/rides/show.blade.php
+++ resources/views/profile/rides/show.blade.php
@@ -3,6 +3,8 @@
 {{--        <x-my-rides-card :ride="$ride" />--}}
         @if( $currentTab === 'my-rides')
             <x-my-ride :ride="$ride" />
+        @elseif($currentTab === 'cancelled-rides')
+            <x-my-ride :ride="$ride" />
         @endif
 
     </div>
